ef54b86415392a406ae68c653e5cea094969e1cd
Pontiac76 <pontiac76@gmail.com>
2025-12-17 21:45:47 -0500

    Testing

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
+
+#include <conio.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <peekpoke.h>
+#include <string.h>
+#include <c64.h>
+#include <cbm_petscii_charmap.h>
+
+#include "common.h"
+//#link "common.c"
+
+typedef struct {
+  char bin;
+  char time;
+} job;
+
+typedef struct {
+  job task[50];
+} jobs;
+
+jobs TheJobs[5];
 
 void main(void) {
-  printf("Hello World!\n");
+  printf("\nHello World!\n");
+  //getchar();
 }
